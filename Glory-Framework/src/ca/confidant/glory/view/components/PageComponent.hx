9149baab0082737a45795927673932cc55123f41
Allan Dowdeswell <secrets@confidant.ca>
2012-08-24 14:12:20 -0600

    Added PageComponent.

---
+++ Glory-Framework/src/ca/confidant/glory/view/components/PageComponent.hx
@@ -0,0 +1,15 @@
+package ca.confidant.glory.view.components;
+import nme.display.Sprite;
+
+/**
+ * PageComponents hold the actors. 
+ * Make one then add a bitmap or svg.
+ */
+class PageComponent extends Sprite {
+	
+
+	public function new () {
+		super ();
+		trace("new Page!");
+	}
+}
